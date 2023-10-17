9bc0804cb61a2e241320b15dcde358650a633cfa
Stefan Turcu <stefan.turcu23@gmail.com>
2023-10-17 22:14:22 +0300

    Section 8: User Registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,42 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
-
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'Testing error')
-        return redirect('register')
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check if passwords match
+        if password == password2:
+            # check usrename
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username already exists')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'Email already exists')
+                    return redirect('register')
+                else:
+                    # create user
+                    user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+
+                    # login after register
+                    # auth.login(request, user)
+                    # messages.success(request, 'You are now loged in')
+                    # return redirect('index')
+                    
+                    user.save()
+                    messages.success(request, 'You are now registered and can now log in')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Password do not match')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
@@ -14,9 +45,8 @@ def login(request):
         # LogIn User
         return
     else:
-        return render(request, 'accounts/register.html')
+        return render(request, 'accounts/login.html')
 
-    return render(request, 'accounts/login.html')
 
 def logout(request):
     return redirect('index')
